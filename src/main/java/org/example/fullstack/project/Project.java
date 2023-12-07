25ba2d9f22ff0e5ef0d68478636712333c191a8f
NikolayDemekhin <Nescafe.gold.1988@mail.ru>
2023-12-07 18:45:23 +0300

    added persistence

---
+++ src/main/java/org/example/fullstack/project/Project.java
@@ -0,0 +1,31 @@
+package org.example.fullstack.project;
+
+import io.quarkus.hibernate.reactive.panache.PanacheEntity;
+import jakarta.persistence.*;
+import org.example.fullstack.user.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "projects",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames ={"name", "user_id"})
+    })
+public class Project extends PanacheEntity {
+
+    @Column(nullable = false)
+    public String name;
+
+    @ManyToOne(optional = false)
+    public User user;
+
+    @CreationTimestamp
+    @Column(updatable = false, nullable = false)
+    public ZonedDateTime created;
+
+    @Version
+    public int version;
+
+
+}
