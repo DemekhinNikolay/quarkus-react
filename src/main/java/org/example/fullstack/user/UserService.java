548f6ae93ce50842dd2505e284c72e78842c15f3
NikolayDemekhin <Nescafe.gold.1988@mail.ru>
2023-12-09 16:32:33 +0300

    creating http api orig

--- src/main/java/org/example/fullstack/user/UserService.java
+++ src/main/java/org/example/fullstack/user/UserService.java
@@ -1,2 +1,58 @@
-package org.example.fullstack.user;public class UserService {
+package org.example.fullstack.user;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
+import io.quarkus.hibernate.reactive.panache.common.WithSession;
+import io.quarkus.hibernate.reactive.panache.common.WithTransaction;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.example.fullstack.project.Project;
+import org.example.fullstack.task.Task;
+import org.hibernate.ObjectNotFoundException;
+import java.util.List;
+
+@ApplicationScoped
+@WithSession
+public class UserService {
+
+    public Uni<User> findById(long id) {
+        return User.<User>findById(id)
+                .onItem().ifNull().failWith(() -> new ObjectNotFoundException(id, "User"));
+    }
+
+    public Uni<User> findByName(String name) {
+        return User.find("name", name).firstResult();
+    }
+
+    public Uni<List<User>> list() {
+        return User.listAll();
+    }
+
+    @WithTransaction
+    public Uni<User> create(User user) {
+        user.password = BcryptUtil.bcryptHash(user.password);
+        return user.persistAndFlush();
+    }
+
+    @WithTransaction
+    public Uni<User> update(User user) {
+        return findById(user.id)
+                .chain(u -> User.getSession())
+                .chain(s -> s.merge(user));
+    }
+
+    @WithTransaction
+    public Uni<Void> delete(long id) {
+        return findById(id)
+                .chain(u -> Uni.combine().all().unis(
+                        Task.delete("user.id", u.id),
+                        Project.delete("user.id", u.id)
+                ).asTuple()
+                                .chain(t -> u.delete())
+                );
+    }
+
+    public Uni<User> getCurrentUser() {
+        return User.find("order by ID").firstResult();
+    }
+
 }
